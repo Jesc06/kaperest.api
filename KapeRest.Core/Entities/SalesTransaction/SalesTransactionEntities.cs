626d84d5956d3f7fd56e8685671128142c5ef79c
Joshuaesc <escarezjohnjoshuamanalo@gmail.com>
2025-10-29 20:39:58 +0800

    added supplier entities

--- KapeRest.Core/Entities/SalesTransaction/SalesTransactionEntities.cs
+++ KapeRest.Core/Entities/SalesTransaction/SalesTransactionEntities.cs
@@ -8,6 +8,19 @@ namespace KapeRest.Core.Entities.SalesTransaction
 {
     public class SalesTransactionEntities
     {
+        public int Id { get; set; }
+        public string ReceiptNumber { get; set; } = Guid.NewGuid().ToString().Substring(0, 8).ToUpper();
+        public DateTime DateTime { get; set; } = DateTime.Now;
 
+        public string CashierId { get; set; } = string.Empty;  
+        public int BranchId { get; set; }                    
+
+        public decimal Subtotal { get; set; }
+        public decimal Tax { get; set; }
+        public decimal Discount { get; set; }
+        public decimal Total { get; set; }
+
+        public string PaymentMethod { get; set; } = "Cash";   
+        public bool IsHold { get; set; } = false;
     }
 }
---
+++ KapeRest.Infrastructure/Persistence/Migrations/20251029123331_addedSupllierTransactionHistory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KapeRest.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedSupllierTransactionHistory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
