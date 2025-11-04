3d7c817c6005d60045898ef47dd56c69e4855b0f
Joshuaesc <escarezjohnjoshuamanalo@gmail.com>
2025-11-04 21:31:37 +0800

    nag move on inalis ang folder na di need

---
+++ KapeRest.Application/Interfaces/PayMongo/IPayMongo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KapeRest.Application.Interfaces.PayMongo
+{
+    public class IPayMongo
+    {
+    }
+}
--- KapeRest.Application/Services/PayMongo/PayMongoService.cs
+++ KapeRest.Application/Services/PayMongo/PayMongoService.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KapeRest.Application.Interfaces.GeneratePdf
+namespace KapeRest.Application.Services.PayMongo
 {
-    public interface ISalesPdf
+    public class PayMongoService
     {
-        byte[] GeneratePdf<T>(T document);
     }
 }
